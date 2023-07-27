01ab2488a79d8662ebbfcff30e0148fbc6a5c36d
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2023-07-27 23:05:04 +0530

    This problem solve from codequotient bit manipulation

---
+++ day52/day52.java
@@ -0,0 +1,22 @@
+//problem solve from codequotient: save the earth with fever
+public class day52 {
+    // Return the minimum number of jumps
+    public static int getMinJumps(unsigned long long int n) {
+        // Write your code here
+        // this question is about converting the n as binary and counting the number of 1 present there
+        int c = 0;
+        while(n!=0)
+        {
+            if(n%2==1)
+            {
+                c++;
+            }
+            n /=2;
+        }
+        return c;
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
