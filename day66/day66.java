97585412364f77e52745c77e67dd8d814b4d776b
Chandan Kumar Mahato <cmhandan@gmail.com>
2023-08-17 22:33:46 +0530

    this is 5th day coding ninjas

---
+++ day66/day66.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static int sumFirstN(int n) {
+        // Write Yof
+        int sum = 0;
+        for(int a=1;a<=n;a++)
+        {
+            sum +=a;
+        }
+        return sum;
+    }
+}
