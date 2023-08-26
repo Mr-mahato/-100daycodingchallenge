d90dcb715c76cd663f4de9f32831d41ca2e2a134
Chandan Kumar Mahato <cmhandan@gmail.com>
2023-08-26 20:59:30 +0530

    This is the next day problem of coding ninjas

---
+++ day75/day75.java
@@ -0,0 +1,13 @@
+import java.util.* ;
+import java.io.*; 
+
+public class day75 {
+    static void printName(String s){
+        // Write your code here.
+        int n = 5;
+        while(n-->0)
+        {
+            System.out.println(s);
+        }
+    }
+}
