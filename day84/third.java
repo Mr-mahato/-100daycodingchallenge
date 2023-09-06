9e6fa091fc7db68c405d672b462521ad409235b4
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2023-09-06 21:25:46 +0530

    This is the third problem in coding contest in codechef

---
+++ day84/third.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+// third coding problem in codechef
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		String str = new String();
+		str = sc.next();
+		System.out.println(str.charAt(6));
+	    
+	}
+}
